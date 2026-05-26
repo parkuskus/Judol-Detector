242d61dd1ac648100ec7d738c9ec6901a04ebb95
Jason Edward Salim <jasonedwardsalim@gmail.com>
2026-05-26 19:36:38 +0700

    feat: detection for each algorithms timing

--- src/algorithms/index.ts
+++ src/algorithms/index.ts
@@ -3,36 +3,62 @@ import { createBoyerMooreEngine } from './boyerMoore';
 import { createFuzzyEngine } from './weightedLevenshtein';
 import { createKmpEngine } from './kmp';
 import { createRegexEngine } from './regex';
-import { normalizeKeyword } from './shared';
+import { normalizeKeyword, nowMs } from './shared';
+
+export interface DetectionTimings {
+	kmp: number;
+	bm: number;
+	regex: number;
+	fuzzy: number;
+}
 
 export async function runDetectionEngines(context: DetectionContext): Promise<KeywordMatch[]> {
-	const matches: KeywordMatch[] = [];
+	const { matches } = await runDetectionEnginesDetailed(context);
+	return matches;
+}
 
+export async function runDetectionEnginesDetailed(context: DetectionContext): Promise<{ matches: KeywordMatch[]; timings: DetectionTimings }> {
 	const kmpEngine = createKmpEngine();
 	const boyerMooreEngine = createBoyerMooreEngine();
 	const regexEngine = createRegexEngine();
 	const fuzzyEngine = createFuzzyEngine();
 
+	const kmpStart = nowMs();
 	const kmpMatches = await kmpEngine.detect(context);
-	const bmMatches = await boyerMooreEngine.detect(context);
-	const exactKeywords = new Set<string>();
+	const kmpTime = nowMs() - kmpStart;
 
+	const bmStart = nowMs();
+	const bmMatches = await boyerMooreEngine.detect(context);
+	const bmTime = nowMs() - bmStart;
+
+	const exactKeywords = new Set<string>();
 	for (const match of [...kmpMatches, ...bmMatches]) {
 		exactKeywords.add(normalizeKeyword(match.keyword));
 	}
 
+	const regexStart = nowMs();
 	const regexMatches = await regexEngine.detect(context);
+	const regexTime = nowMs() - regexStart;
+
+	const fuzzyStart = nowMs();
 	const fuzzyMatches = await fuzzyEngine.detect({
 		...context,
 		exactMatchedKeywords: exactKeywords
 	});
+	const fuzzyTime = nowMs() - fuzzyStart;
 
+	const matches: KeywordMatch[] = [];
 	[kmpMatches, bmMatches, regexMatches, fuzzyMatches].forEach((engineMatches) => {
-		matches.push(...engineMatches.map((match) => ({
-			...match,
-			algorithm: match.algorithm
-		})));
+		matches.push(...engineMatches.map((match) => ({ ...match })));
 	});
 
-	return matches;
+	return {
+		matches,
+		timings: {
+			kmp: kmpTime,
+			bm: bmTime,
+			regex: regexTime,
+			fuzzy: fuzzyTime
+		}
+	};
 }
